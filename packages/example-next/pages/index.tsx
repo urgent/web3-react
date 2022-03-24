c488ede22d51dc03c2eed873ec4d6028a5178549
urgent <urgent.programmer@gmail.com>
2022-03-24 17:58:52 -0400

    State for nft swap

--- packages/example-next/pages/index.tsx
+++ packages/example-next/pages/index.tsx
@@ -1,19 +1,28 @@
-import CoinbaseWalletCard from '../components/connectors/CoinbaseWalletCard'
+import {useState, useEffect} from 'react'
 import MetaMaskCard from '../components/connectors/MetaMaskCard'
-import NetworkCard from '../components/connectors/NetworkCard'
-import WalletConnectCard from '../components/connectors/WalletConnectCard'
 import ProviderExample from '../components/ProviderExample'
+import { NftSwapV4 } from '@traderxyz/nft-swap-sdk'
+import { hooks, metaMask } from '../connectors/metaMask'
 
 export default function Home() {
+  const [nftSwap, setNftSwap] = useState<boolean | NftSwapV4>(false)
+  const { useChainId, useProvider } = hooks
+  const chainId = useChainId()
+  const provider = useProvider()
+  
+  useEffect(()  => {
+    if(provider) {
+      setNftSwap(new NftSwapV4(provider, provider.getSigner(), chainId as number))
+    }
+  }, [provider, chainId])
+  
   return (
     <>
       <ProviderExample />
       <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
         <MetaMaskCard />
-        <WalletConnectCard />
-        <CoinbaseWalletCard />
-        <NetworkCard />
       </div>
+      <button onClick={(()=>{ console.log(nftSwap)})}>test</button>
     </>
   )
 }
