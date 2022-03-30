812be13832b4023f8b6753768d6c84bbe4371534
urgent <urgent.programmer@gmail.com>
2022-03-29 21:26:55 -0400

    Deploy smart contract for minting NFT

---
+++ packages/example-next/contracts/nft.sol
@@ -0,0 +1,34 @@
+//Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract MyNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFT", "NFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function mintToken(address to, uint256 tokenId, string memory uri) public virtual payable {
+        require(msg.value >= 10, "Not enough ETH sent; check price!"); 
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+}
